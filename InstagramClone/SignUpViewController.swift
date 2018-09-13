ef100af9a08f625082d787a5f749466566e818e5
Alex T <alextsai0227@gmail.com>
2018-09-14 00:51:15 +1000

    structure of instagram clone app

---
+++ InstagramClone/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  InstagramClone
+//
+//  Created by 蔡舜珵 on 2018/9/13.
+//  Copyright © 2018年 蔡舜珵. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
