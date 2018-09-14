ca9ca21a3303addb907f6a0a889f5bdb3a66b6f0
Alex T <alextsai0227@gmail.com>
2018-09-15 00:11:26 +1000

    import firebase to implemet sign up & sign in function

--- InstagramClone/SignInViewController.swift
+++ InstagramClone/SignInViewController.swift
@@ -7,29 +7,44 @@
 //
 
 import UIKit
+import Firebase
+
 
 class SignInViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loginBtn.isEnabled = false
+        handleTextfield()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func handleTextfield(){
+        emailTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func textFieldDidChange(){
+        if let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty{
+            loginBtn.isEnabled = true
+        }else{
+            loginBtn.isEnabled = false
+        }
     }
-    */
 
+    @IBAction func loginBtn_TouchUpInside(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {(user, err) in
+            if err != nil{
+                print(err?.localizedDescription)
+                return
+            }
+            self.performSegue(withIdentifier: "singInToTabBarVC", sender: nil)
+        })
+    
+    }
+    
 }
--- InstagramClone/SignUpViewController.swift
+++ InstagramClone/SignUpViewController.swift
@@ -7,32 +7,86 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var userNameTextFIeld: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var signUpBtn: UIButton!
+    var selectedImage: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SignUpViewController.selectProfileImage))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
+        profileImage.layer.cornerRadius = 50
+        profileImage.clipsToBounds = true
+        signUpBtn.isEnabled = false
+        handleTextfield()
+    }
+    
+    func handleTextfield(){
+        userNameTextFIeld.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        emailTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControlEvents.editingChanged)
+    
+    }
+    
+    @objc func textFieldDidChange(){
+        if let username = userNameTextFIeld.text, !username.isEmpty, let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty{
+            signUpBtn.isEnabled = true
+        }else{
+            signUpBtn.isEnabled = false
+        }
+    }
+    @objc func selectProfileImage() {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
     }
     @IBAction func dismiss_onClick(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signUpBtn_TouchUpInside(_ sender: Any) {
+        if userNameTextFIeld.text != nil &&  emailTextField.text != nil && passwordTextField.text != nil {
+            FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                    return
+                }
+                let uid = user?.uid
+                let storageRef = FIRStorage.storage().reference(forURL: "gs://instagramclone-94872.appspot.com").child("profile_image").child(uid!)
+                if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1){
+                    storageRef.put(imageData, metadata: nil, completion: {(metadata, err) in
+                        if err != nil {
+                            return
+                        }
+                        let profileImageUrl = metadata?.downloadURL()?.absoluteString
+                        let ref = FIRDatabase.database().reference()
+                        let userReference = ref.child("users")
+                        let newUserReference = userReference.child(uid!)
+                        newUserReference.setValue(["username": self.userNameTextFIeld.text!, "email": self.emailTextField.text!, "profileImgUrl": profileImageUrl])
+                    })
+                }
+                self.performSegue(withIdentifier: "singUpToTabBarVC", sender: nil)
+            })
+        }
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            selectedImage = image
+            profileImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
-    */
 
 }
